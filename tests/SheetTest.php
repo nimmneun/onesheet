ee4360d3bd6be1bccba984843f6a3ddcd57a4d21
Stefan <nimmneun@gmail.com>
2016-07-09 23:39:31 +0200

    more tests + scrutinizer config

--- tests/SheetTest.php
+++ tests/SheetTest.php
@@ -7,19 +7,47 @@ class SheetTest extends PHPUnit_Framework_TestCase
      */
     protected static $sheet;
 
+    /**
+     * @var string
+     */
+    private static $path;
+
     public static function setUpBeforeClass()
     {
         self::$sheet = new \OneSheet\Sheet('A2');
+        self::$path = sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'sheet1.xml';
+    }
+
+    public function testConstructor()
+    {
+        $this->assertInstanceOf('OneSheet\Sheet', new OneSheet\Sheet('B2'));
     }
 
     public function testSheetFilePath()
     {
-        $this->assertEquals(sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'sheet1.xml', self::$sheet->sheetFilePath());
+        $this->assertEquals(
+            self::$path, self::$sheet->sheetFilePath()
+        );
     }
 
     public function testAddStyle()
     {
-        $style = $this->getMock('OneSheet\\Style');
-        $this->assertGreaterThan(1, \OneSheet\StyleHelper::buildStyle($style));
+        $this->assertGreaterThan(1, \OneSheet\StyleHelper::buildStyle(new OneSheet\Style()));
     }
+
+    public function testAddRows()
+    {
+        $number = time();
+        $string = uniqid();
+
+        self::$sheet->addRows(array(array($number)), 1);
+        $style = new \OneSheet\Style();
+        self::$sheet->addRows(array(array($string)), $style);
+        $xml = file_get_contents(self::$path);
+
+        $this->assertEquals(1, preg_match('~'. $number . '.*' . $string . '~', $xml, $match));
+    }
+
+
+
 }
