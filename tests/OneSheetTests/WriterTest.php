192332b1e6b438a80f62d6900836da485736fbe9
Stefan <nimmneun@gmail.com>
2016-08-16 19:34:41 +0200

    Improved writer testing.

--- tests/OneSheetTests/WriterTest.php
+++ tests/OneSheetTests/WriterTest.php
@@ -29,4 +29,60 @@ class WriterTest extends \PHPUnit_Framework_TestCase
         $writer = new Writer();
         $writer->addRows(array(1,2,3,4,5));
     }
+
+    public function testSetFreezePaneCellId()
+    {
+        $writer = new Writer();
+        $writer->setfreezePaneCellId('A5');
+
+        $sheet = $this->getObjectProperty($writer, 'sheet');
+        $value = $this->getObjectProperty($sheet, 'freezePaneId');
+
+        $this->assertEquals('A5', $value);
+    }
+
+    public function testEnableCellAutosizing()
+    {
+        $writer = new Writer();
+        $writer->enableCellAutosizing();
+
+        $sheet = $this->getObjectProperty($writer, 'sheet');
+        $value = $this->getObjectProperty($sheet, 'useCellAutosizing');
+
+        $this->assertEquals(true, $value);
+    }
+
+    public function testDisableCellAutosizing()
+    {
+        $writer = new Writer();
+        $writer->enableCellAutosizing();
+        $writer->disableCellAutosizing();
+
+        $sheet = $this->getObjectProperty($writer, 'sheet');
+        $value = $this->getObjectProperty($sheet, 'useCellAutosizing');
+
+        $this->assertEquals(false, $value);
+    }
+
+    public function testSetFixedColumnWidths()
+    {
+        $widths = array(9, 10, 11);
+
+        $writer = new Writer();
+        $writer->setFixedColumnWidths($widths);
+
+        $sheet = $this->getObjectProperty($writer, 'sheet');
+        $value = $this->getObjectProperty($sheet, 'columnWidths');
+
+        $this->assertEquals($widths, $value);
+        $this->assertEquals(30, array_sum($value));
+    }
+
+    private function getObjectProperty($object, $propertyName)
+    {
+        $reflection = new \ReflectionClass($object);
+        $property = $reflection->getProperty($propertyName);
+        $property->setAccessible(true);
+        return $property->getValue($object);
+    }
 }
